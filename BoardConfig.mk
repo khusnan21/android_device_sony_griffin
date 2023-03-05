04defc7fd31c1479a3097fcacb2aacd940237ccc
khusnan21 <khusnan21@gmail.com>
2023-03-05 23:42:46 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/sony/bahamut
+DEVICE_PATH := device/sony/griffin
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
